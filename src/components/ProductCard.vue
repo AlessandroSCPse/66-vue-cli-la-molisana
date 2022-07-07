ed7fac2cd8cc1a8055b1888fc7cff67e5c5dbb0d
Alessandro Scolozzi <scolozzi.alessandro@gmail.com>
2022-07-07 10:55:11 +0200

    cars

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="product-card">
+        <img :src="productDetails.src" :alt="productDetails.titolo">
+        <h2>{{ productDetails.titolo }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ProductCard",
+    props: {
+        productDetails: Object
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/variables';
+
+.product-card {
+    background-color: white;
+    padding: 12px;
+    width: calc((100% / 4) - 6px);
+    margin: 3px;
+
+    h2 {
+        color: $brand_main_color;
+    }
+}
+</style>
--- src/components/ProductsList.vue
+++ src/components/ProductsList.vue
@@ -1,14 +1,27 @@
 <template>
     <section>
         <div class="container">
-            Lista prodotti
+            <div class="products-list">
+                <ProductCard v-for="element, index in products" :key="index" :productDetails="element" />
+            </div>
         </div>
     </section>
 </template>
 
 <script>
+import productsJson from '../assets/la-molisana.json';
+import ProductCard from './ProductCard.vue';
+
 export default {
-    name: "ProductsList"
+    name: "ProductsList",
+    components: {
+        ProductCard
+    },
+    data() {
+        return {
+            products: productsJson
+        };
+    }
 }
 </script>
 
@@ -16,5 +29,10 @@ export default {
 section {
     padding: 30px 0;
     background-image: url('../assets/img/fondo-pag-speciali.jpg');
+
+    .products-list {
+        display: flex;
+        flex-wrap: wrap;
+    }
 }
 </style>
